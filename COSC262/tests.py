a0535a51365b7d0f748638492dbd7aa8b3462f46
chalos18 <63985831+chalos18@users.noreply.github.com>
2024-05-06 15:44:32 +1200

    new assigment

--- COSC262/tests.py
+++ COSC262/tests.py
@@ -361,3 +361,41 @@ U 5
 
 # print(dijkstra(adjacency_list(graph_string), 0))
 # print(dijkstra(adjacency_list(graph_string), 2))
+
+
+def coins_reqd(value, coinage):
+    """Minimum number of coins to represent value.
+    Assumes there is a 1-unit coin."""
+    num_coins = [0] * (value + 1)
+    for amt in range(1, value + 1):
+        num_coins[amt] = 1 + min(num_coins[amt - c] for c in coinage if c <= amt)
+
+    print(num_coins)
+    # The value of the num_coins array is displayed at this point.
+    return num_coins[value]
+
+# print(coins_reqd(19, [1,5,7,11]))
+
+
+def coins_reqd(value, coinage):
+    """A version that doesn't use a list comprehension"""
+    num_coins = [0] * (value + 1)
+    for amt in range(1, value + 1):
+        minimum = None
+        for c in coinage:
+            if c <= amt:
+                coin_count = num_coins[
+                    amt - c
+                ]  # Num coins required to solve for amt - c
+                if minimum is None or coin_count < minimum:
+                    minimum = coin_count
+        num_coins[amt] = 1 + minimum
+    print(num_coins)
+    return num_coins[value]
+
+
+coinage = [1, 10, 25]
+amount = 30
+answer = coins_reqd(amount, coinage)
+
+print(answer)
