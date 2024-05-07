7bfaf8bb712b5968a0decde5f0af78886c4c2534
chalos18 <63985831+chalos18@users.noreply.github.com>
2024-05-07 22:35:22 +1200

    new tests

--- COSC262/tests.py
+++ COSC262/tests.py
@@ -374,12 +374,20 @@ def coins_reqd(value, coinage):
     # The value of the num_coins array is displayed at this point.
     return num_coins[value]
 
+
 # print(coins_reqd(19, [1,5,7,11]))
+# coinage = [1, 10, 25]
+# amount = 30
+# answer = coins_reqd(amount, coinage)
+
+# print(answer)
 
 
 def coins_reqd(value, coinage):
-    """A version that doesn't use a list comprehension"""
+    """A version that keeps track of the coinage used"""
     num_coins = [0] * (value + 1)
+    coinage_used = [[] for _ in range(value + 1)]
+
     for amt in range(1, value + 1):
         minimum = None
         for c in coinage:
@@ -389,13 +397,77 @@ def coins_reqd(value, coinage):
                 ]  # Num coins required to solve for amt - c
                 if minimum is None or coin_count < minimum:
                     minimum = coin_count
+                    coinage_used[amt] = coinage_used[amt - c] + [c]
         num_coins[amt] = 1 + minimum
-    print(num_coins)
-    return num_coins[value]
+
+    # print(coinage_used)
+    coin_counts = {}
+    for coin in coinage_used[value]:
+        coin_counts[coin] = coin_counts.get(coin, 0) + 1
+
+    return sorted(coin_counts.items(), reverse=True)
 
 
+# For example, a call to coins_reqd(32, [1, 10, 25]) should return [(10, 3), (1, 2)].
 coinage = [1, 10, 25]
-amount = 30
-answer = coins_reqd(amount, coinage)
+amount = 32
+# answer = coins_reqd(amount, coinage)
 
-print(answer)
+# print(answer)
+
+"""A broken implementation of a recursive search for the optimal path through
+   a grid of weights.
+   Richard Lobb, January 2019.
+"""
+INFINITY = float("inf")  # Same as math.inf
+
+
+def read_grid(filename):
+    """Read from the given file an n x m grid of integer weights.
+    The file must consist of n lines of m space-separated integers.
+    n and m are inferred from the file contents.
+    Returns the grid as an n element list of m element lists.
+    THIS FUNCTION DOES NOT HAVE BUGS.
+    """
+    with open(filename) as infile:
+        lines = infile.read().splitlines()
+
+    grid = [[int(bit) for bit in line.split()] for line in lines]
+    return grid
+
+
+def grid_cost(grid):
+    """The cheapest cost from row 1 to row n (1-origin) in the given
+    grid of integer weights.
+    """
+    n_rows = len(grid)
+    n_cols = len(grid[0])
+
+    def cell_cost(row, col):
+        """The cost of getting to a given cell in the current grid."""
+        if row < 0 or row >= n_rows or col < 0 or col >= n_cols:
+            return INFINITY  # Off-grid cells are treated as infinities
+        else:
+            cost = grid[row][col]
+            if row != 0:
+                cost += min(
+                    cell_cost(row - 1, col + delta_col) for delta_col in range(-1, 2)
+                )
+            return cost
+
+    best = min(cell_cost(n_rows -1, col) for col in range(n_cols))
+    return best
+
+
+def file_cost(filename):
+    """The cheapest cost from row 1 to row n (1-origin) in the grid of integer
+    weights read from the given file
+    """
+    return grid_cost(read_grid(filename))
+
+
+# 3
+print(file_cost("COSC262/checkerboard_trivial.txt"))
+
+# 8
+print(file_cost("COSC262/checkerboard_small.txt"))
