85e8f728b965e41abc5c4133e5220ed77d0c8b05
chalos18 <63985831+chalos18@users.noreply.github.com>
2024-05-20 21:00:04 +1200

    new quiz

---
+++ COSC262/computational_geometry.py
@@ -0,0 +1,380 @@
+# an example related to computational geometry
+
+import matplotlib.pyplot as plt
+from graphviz import Graph
+
+# # First some simple text output.
+# vertices = [(0, 0), (100, 0), (1, 50), (100, 100), (0, 100), (0,0)]
+# vx, vy = zip(*vertices)  # Unpack them
+# points = [(1, 1), (20, 20), (20, 80), (60, 50),
+#      (97, 1), (1, 48), (1, 52), (97, 99), (1, 99)]
+# px, py = zip(*points) # Unpack
+# print("Vertex x values:", vx)
+# print("Vertex y values:", vy)
+# print("Point x values:", px)
+# print("Point y values:", py)
+
+# # Now a matplotlib graph
+# axes = plt.axes()
+# axes.plot(vx, vy, color='blue', marker='o', linestyle='--')
+# axes.plot(px, py, color='red', marker='x', linestyle='')
+# axes.set_title('The example from the geometry lecture notes')
+
+
+# g = Graph()
+# g.node('Root', '23')
+# g.node('Leaf1', '13', shape='box')
+# g.node('Leaf2', '99', shape='box')
+# g.edge('Root', 'Leaf1')
+# g.edge('Root', 'Leaf2')
+# g.render('graph', format='png', view=False)
+
+
+class Vec:
+    """A simple vector in 2D. Also used as a position vector for points"""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vec(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vec(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, scale):
+        """Multiplication by a scalar"""
+        return Vec(self.x * scale, self.y * scale)
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y
+
+    def lensq(self):
+        return self.dot(self)
+
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
+
+
+def signed_area(a, b, c):
+    """Twice the area of the triangle abc.
+    Positive if abc are in counter clockwise order.
+    Zero if a, b, c are colinear.
+    Otherwise negative.
+    """
+    p = b - a
+    q = c - a
+    return p.x * q.y - q.x * p.y
+
+
+def is_ccw(a, b, c):
+    """True iff triangle abc is counter-clockwise."""
+    p = b - a
+    q = c - a
+    area = p.x * q.y - q.x * p.y
+	 # May want to throw an exception if area == 0
+    return area > 0 
+
+
+def is_on_segment(p, a, b):
+    # Check if p is collinear with a and b --> 0 if collinear
+    if signed_area(a, b, p) != 0:
+        return False
+
+    # Check if p is within the bounding box of a and b
+    if min(a.x, b.x) <= p.x <= max(a.x, b.x) and min(a.y, b.y) <= p.y <= max(a.y, b.y):
+        return True
+    else:
+        return False
+
+
+def classify_points(line_start, line_end, points):
+    """Returns a two tuple of integers.
+    The first element of the tuple is the number of points from the points list that
+    lie to the right of the given line, looking from start to end and
+    the second element of the tuple is the number of points to the left of the line.
+    The line is assumed to extend infinitely in both directions.
+    You may assume that no points lie exactly on the line.
+    All points are of the type Vec with integer coordinates as defined in the lecture notes
+    """
+    left = 0
+    right = 0
+    for point in points:
+        left_expression = (point.x - line_start.x) * (line_end.y - line_start.y)
+        right_expression = (point.y - line_start.y) * (line_end.x - line_start.x)
+        expression = left_expression - right_expression
+        if expression < 0:
+            left += 1
+        else:
+            right += 1
+
+    return (right, left)
+
+def intersecting(a, b, c, d) :
+    """ Returns True if the line segment from a to b intersects the line segment from c to d. 
+    Otherwise the function returns False. 
+    The parameters a, b, c and d are all points of type Vec as defined in the lecture notes. 
+    Coordinates are all integers.
+    """
+    is_intersecting = is_ccw(a, d, b) != is_ccw(a, c, b)
+    different_sides = is_ccw(c, a, d) != is_ccw(c, b, d)
+    
+    return is_intersecting and different_sides
+
+
+def is_strictly_convex(vertices):
+    """ Takes a list of three or more points, each of type Vec as in the lecture notes, and returns True if and only if the vertices, 
+    taken in the given order, define a strictly-convex counter-clockwise polygon. 
+    Otherwise the function returns False.
+    While a convex polygon can have interior angles equal to 180o, a strictly-convex polygon has all interior angles strictly less than 180o. 
+    """
+    n = len(vertices)
+    if n < 3:
+        return False  # Needs a min of 3 to form a polygon
+
+    for i in range(n):
+        a = vertices[i]
+        b = vertices[(i + 1) % n]
+        c = vertices[(i + 2) % n]
+        
+        if not is_ccw(a, b, c):
+            return False
+
+    return True
+
+        
+def gift_wrap(points):
+    """ Returns points on convex hull in CCW using the Gift Wrap algorithm"""
+    # Get the bottom-most point (and left-most if necessary).
+    assert len(points) >= 3
+    bottommost = min(points, key=lambda p: (p.y, p.x))
+    hull = [bottommost]
+    done = False
+    
+    # Loop, adding one vertex at a time, until hull is (about to be) closed.
+    while not done:
+        candidate = None
+        # Loop through all points, looking for the one that is "rightmost"
+        # looking from last point on hull
+        for p in points:
+            if p is hull[-1]:
+                continue
+            if candidate is None or is_ccw(candidate, hull[-1], p):
+                candidate = p
+        if candidate is bottommost:
+            done = True    # We've closed the hull
+        else:
+            hull.append(candidate)
+
+    return hull
+
+
+points = [
+    Vec(1, 99),
+    Vec(0, 100),
+    Vec(50, 0),
+    Vec(50, 1),
+    Vec(50, 99),
+    Vec(50, 50),
+    Vec(100, 100),
+    Vec(99, 99)
+    ]
+verts = gift_wrap(points)
+# for v in verts:
+    # print(v)
+
+
+points = [
+    Vec(1, 1),
+    Vec(99, 1),
+    Vec(100, 100),
+    Vec(99, 99),
+    Vec(0, 100),
+    Vec(100, 0),
+    Vec(1, 99),
+    Vec(0, 0),
+    Vec(50, 50)]
+verts = gift_wrap(points)
+# for v in verts:
+    # print(v)
+
+
+class PointSortKey:
+    """A class for use as a key when sorting points wrt bottommost point"""
+    def __init__(self, p, bottommost):
+        """Construct an instance of the sort key"""
+        self.direction = p - bottommost
+        self.is_bottommost = self.direction.lensq() == 0  # True if p == bottommost
+        
+    def __lt__(self, other):
+        """Compares two sort keys. p1 < p2 means the vector the from bottommost point
+           to p2 is to the left of the vector from the bottommost to p1.
+        """
+        if self.is_bottommost:
+            return True   # Ensure bottommost point is less than all other points
+        elif other.is_bottommost:
+            return False  # Ensure no other point is less than the bottommost
+        else:
+            area = self.direction.x * other.direction.y - other.direction.x * self.direction.y
+            return area > 0
+
+
+def plot_hull(points, hull):
+    """Plot the given set of points and the computed convex hull"""
+    plt.scatter([p.x for p in points], [p.y for p in points])
+    plt.plot([v.x for v in hull + [hull[0]]], [v.y for v in hull + [hull[0]]])
+    plt.show()
+
+
+def plot_poly(points):
+    """Plot the given set of points as a closed polygon"""
+    plt.plot([v.x for v in points + [points[0]]], [v.y for v in points + [points[0]]])
+    plt.show()
+
+
+def simple_polygon(points):
+    """Takes a list of points (each of type Vec) and returns a simple polygon that passes through all points. 
+    You must use the algorithm in the notes, so that the first point will be the bottom-most (and, if necessary, left-most) point 
+    and the other points follow in counter-clockwise order. 
+    The return value should be a list of points of type Vec.
+    """
+    bottommost = min(points, key=lambda p: (p.y, p.x))
+    simply_poly = sorted(points, key=lambda p: PointSortKey(p, bottommost))
+    return simply_poly
+
+
+def graham_scan(points):
+    bottommost = min(points, key=lambda p: (p.y, p.x))
+    sorted_points = simple_polygon(points)
+    hull = [bottommost, sorted_points[0]]
+
+    for p in sorted_points[1:]:
+        while len(hull) >= 2 and not is_ccw(hull[-2], hull[-1], p):
+            hull.pop()
+        hull.append(p)
+    return hull
+
+
+points = [
+    Vec(100, 100),
+    Vec(0, 100),
+    Vec(50, 0)]
+verts = graham_scan(points)
+for v in verts:
+    print(v)
+
+
+points = [
+    Vec(100, 100),
+    Vec(0, 100),
+    Vec(100, 0),
+    Vec(0, 0),
+    Vec(49, 50)]
+verts = graham_scan(points)
+for v in verts:
+    print(v)
+
+
+points = [
+    Vec(100, 100),
+    Vec(0, 100),
+    Vec(50, 0)]
+verts = simple_polygon(points)
+# for v in verts:
+    # print(v)
+
+
+points = [
+    Vec(100, 100),
+    Vec(0, 100),
+    Vec(100, 0),
+    Vec(0, 0),
+    Vec(49, 50)]
+verts = simple_polygon(points)
+# for v in verts:
+    # print(v)
+
+
+verts = [
+    (0, 0),
+    (100, 0),
+    (100, 100),
+    (0, 100)]
+points = [Vec(v[0], v[1]) for v in verts]
+# print(is_strictly_convex(points))
+
+
+verts = [
+    (0, 0),
+    (0, 100),
+    (100, 100),
+    (100, 0)]
+points = [Vec(v[0], v[1]) for v in verts]
+# print(is_strictly_convex(points))
+
+
+a = Vec(0, 0)
+b = Vec(100, 0)
+c = Vec(101, 1)
+d = Vec(101, -1)
+# print(intersecting(a, b, c, d))
+
+a = Vec(0, 0)
+b = Vec(100, 0)
+c = Vec(99, 1)
+d = Vec(99, -1)
+# print(intersecting(a, b, c, d))
+
+points = [
+    Vec(1, 99),
+    Vec(0, 100),
+    Vec(50, 0),
+    Vec(50, 1),
+    Vec(50, 99),
+    Vec(50, 50),
+    Vec(100, 100),
+    Vec(99, 99),
+]
+
+# print(classify_points(Vec(0, 49), Vec(100, 49), points))
+
+
+points = [
+    Vec(1, 99),
+    Vec(0, 100),
+    Vec(50, 0),
+    Vec(50, 1),
+    Vec(50, 99),
+    Vec(50, 50),
+    Vec(100, 100),
+    Vec(99, 99),
+]
+
+# print(classify_points(Vec(100, 49), Vec(0, 49), points))
+
+a = Vec(1000, 2000)
+b = Vec(0, 0)
+p = Vec(500, 1000)
+# print(is_on_segment(p, a, b))
+
+
+a = Vec(0, 0)
+b = Vec(1000, 2000)
+point_tuples = [
+    (-1, -1),
+    (-1, -2),
+    (-1000, -2000),
+    (0, 0),
+    (1, 2),
+    (500, 1000),
+    (500, 1001),
+    (500, 999),
+    (1000, 2000),
+    (1001, 2001),
+    (1001, 2002),
+    (2000, 4000),
+]
+points = [Vec(p[0], p[1]) for p in point_tuples]
+# for p in points:
+#     print(p, is_on_segment(p, a, b))
